31fef05e74f894dc97a80add1725b0963f9e51e3
Gabriel Almeida <gabrieldcgh@gmail.com>
2025-01-05 01:50:02 -0300

    Docker compose (node+Mysql) completed

--- image-node/index.js
+++ image-node/index.js
@@ -1,6 +1,20 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const config = {
+    host: 'db',
+    user: 'root',
+    password: 'root',
+    database: 'nodedb'
+};
+
+const mysql = require('mysql2');
+
+const connection = mysql.createConnection(config);
+
+const sql = 'INSERT INTO people(name) VALUES ("Gabriel Almeida")';
+connection.query(sql);
+connection.end();
 
 app.get('/', (req, res) => {
     res.send('<h1>Node with Express using Docker Container');
